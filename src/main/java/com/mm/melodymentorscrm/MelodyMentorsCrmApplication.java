bf529bf94c718eb6885af66f0adca4bf26aea380
petersims <ragtimesims@gmail.com>
2023-05-13 10:24:11 -0500

    initial commit - Security Configuration Added

---
+++ src/main/java/com/mm/melodymentorscrm/MelodyMentorsCrmApplication.java
@@ -0,0 +1,13 @@
+package com.mm.melodymentorscrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyMentorsCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelodyMentorsCrmApplication.class, args);
+    }
+
+}
