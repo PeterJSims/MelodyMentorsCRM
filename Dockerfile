35280a14350cafd17168ffe11516418ba7dd8a35
petersims <ragtimesims@gmail.com>
2023-05-14 14:53:08 -0500

    Coding finished --scripts written for automatic upload, bug fixes from unit and integration tests, Dockerfile complete

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,3 @@
 FROM openjdk:17-jdk-slim
-VOLUME /tmp
-
-RUN useradd -d /home/appuser -m -s /bin/bash appuser
-USER appuser
-
-HEALTHCHECK --interval=5m --timeout=3s CMD curl -f http://localhost:8080/actuator/health/ || exit 1
-
-ARG JAR_FILE
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+ADD target/*.jar app.jar
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ build-and-upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# REMEMBER TO SET spring.profiles.active to "prod"
+
+# Clean the /target folder and build a new JAR
+mvn clean verify
+
+#Login to AWS ECR
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/a5c5s3u9
+
+# Build an image of the new JAR from the first step
+docker build -t melodymentors/melodymentorscrm .
+
+# Tag the image
+docker tag melodymentors/melodymentorscrm:latest public.ecr.aws/a5c5s3u9/melodymentors/melodymentorscrm:latest
+
+# Publish the image to AWS
+docker push public.ecr.aws/a5c5s3u9/melodymentors/melodymentorscrm:latest
+
